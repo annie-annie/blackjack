9acf7e219d2d747a6e9015c31463597a8c10ea4a
Annie <anahadjimitova@gmail.com>
2019-04-07 16:10:39 +0100

    Move Card class to own file

---
+++ Card.py
@@ -0,0 +1,10 @@
+class Card():
+    def __init__(self, suite, cardType):
+        self.suite = suite
+        self.cardType = cardType
+
+    def __str__(self):
+        return '{}{}'.format(self.suite, self.cardType)
+
+    def getValue():
+        pass
--- Deck.py
+++ Deck.py
@@ -1,16 +1,5 @@
 from random import randint
-
-
-class Card():
-    def __init__(self, suite, cardType):
-        self.suite = suite
-        self.cardType = cardType
-
-    def __str__(self):
-        return '{}{}'.format(self.suite, self.cardType)
-
-    def getValue():
-        pass
+from Card import Card
 
 
 class Deck():
