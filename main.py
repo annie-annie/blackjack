2f0ec2526ae4875f6d45c137561911b32a6840a0
Annie <anahadjimitova@gmail.com>
2019-04-07 15:11:03 +0100

    Game play rules

---
+++ main.py
@@ -0,0 +1,18 @@
+# Main program
+
+# Deal cards to computer
+# Deal cards to human
+
+# Ask Human hit/stick
+
+# Case stick go to computer
+
+# Case hit >
+# if over computer wins
+# if under Ask Human to hit or stick
+
+# Computer
+# Check if computer has won
+# if not hit & repeat
+# if over player wins
+# If win then win
