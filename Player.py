0add975ad23b8a6500f1122d58779cca709c449d
Annie <anahadjimitova@gmail.com>
2019-05-01 13:42:57 +0300

    Move get total into player class

--- Player.py
+++ Player.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 class Player():
     hand = []
 
@@ -6,3 +9,8 @@ class Player():
 
     def isBust(self):
         return sum(self.hand) > 21
+
+    def getTotal(self):
+        # order for addition needs to be card, then integer or it errors
+        # with `TypeError: unsupported operand type(s) for +: 'int' and 'Card'`
+        return reduce(lambda x, y: y + x, self.hand)
--- main.py
+++ main.py
@@ -17,38 +17,23 @@
 # if over player wins
 # If win then win
 
-from functools import reduce
 
 from Card import Card
 from Deck import Deck
 from Player import Player
 
 
-def getTotal(hand):
-    # order for addition needs to be card, then integer or it errors
-    # with `TypeError: unsupported operand type(s) for +: 'int' and 'Card'`
-    return reduce(lambda x, y: y + x, hand)
-
-
 deck = Deck()
 dealer = Player()
 human = Player()
-
-# Deal cards to dealer
-dealer.hit(deck.getCard())
-dealer.hit(deck.getCard())
 
 # Deal cards to human
 human.hit(deck.getCard())
 human.hit(deck.getCard())
 
 # Check if human won
-# sum(human.hand)
+print(human.hand[0])
+print(human.hand[1])
 
-card1 = deck.getCard()
-card2 = deck.getCard()
 
-print(card1)
-print(card2)
-
-print(getTotal([card1, card2, card1, card2]))
+print(human.getTotal())
