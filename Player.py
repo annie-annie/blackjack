ab483b4cb2dc940087deaabb24fa314454b36534
Annie <anahadjimitova@gmail.com>
2019-05-01 18:10:26 +0300

    Implement ace being 1 and 11 logic

--- Player.py
+++ Player.py
@@ -15,7 +15,13 @@ class Player():
     def getTotal(self):
         # order for addition needs to be card, then integer or it errors
         # with `TypeError: unsupported operand type(s) for +: 'int' and 'Card'`
-        return reduce(lambda x, y: y + x, self.hand)
+        total = reduce(lambda x, y: y + x, self.hand)
+
+        # if gone over but has ace count ace as 1, not 11
+        if total > 21 and self.hasAce():
+            return total - 10
+
+        return total
 
     def has21(self):
         return self.getTotal() == 21
@@ -29,3 +35,10 @@ class Player():
         return print(
             f'{self.name} hand: {cardString}\tTotal: {self.getTotal()}'
         )
+
+    def hasAce(self):
+        for card in self.hand:
+            if card.cardType == 'A':
+                return True
+
+        return False
