755707e971382f8ed1de48c1458c772dd6f59310
Annie <anahadjimitova@gmail.com>
2019-05-18 13:47:11 +0100

    Colour red/green for loose/win

--- GameResult.py
+++ GameResult.py
@@ -6,4 +6,10 @@ class GameResult():
 
     def __str__(self):
         lostOrWonString = "WOW! You win!" if self.humanHasWon else "Oh no, you lost!"
-        return f'{lostOrWonString} \t {self.message}'
+        return self.colourConsoleText(
+            f'{lostOrWonString} \t {self.message}', self.humanHasWon
+        )
+
+    def colourConsoleText(self, text, humanHasWon):
+        colour = "32" if self.humanHasWon else "31"
+        return f"\033[1;{colour};40m {text} \n"
