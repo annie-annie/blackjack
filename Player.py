59fba076fb0044764839b5785461cf0e36d39eaa
Annie <anahadjimitova@gmail.com>
2019-06-03 11:54:47 +0100

    Remove duplicatr function declaration

--- Player.py
+++ Player.py
@@ -8,9 +8,6 @@ class Player():
     def hit(self, card):
         self.hand.append(card)
 
-    def isBust(self):
-        return sum(self.hand) > 21
-
     def getTotal(self):
         # order for addition needs to be card, then integer or it errors
         # with `TypeError: unsupported operand type(s) for +: 'int' and 'Card'`
